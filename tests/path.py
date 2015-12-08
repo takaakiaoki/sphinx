4160f18d27f6f3a81e999794ed4ea780d9139522
Takaaki AOKI <aoki.takaaki@gmail.com>
2015-12-08 18:44:08 +0900

    fix #2158, use io.open() instead of codecs.open() .

--- tests/path.py
+++ tests/path.py
@@ -10,7 +10,7 @@
 import os
 import sys
 import shutil
-from codecs import open
+from io import open
 
 from six import PY2, text_type
 
@@ -140,9 +140,9 @@ class path(text_type):
         """
         Returns the text in the file.
         """
-        f = open(self, mode='U', **kwargs)
+        f = open(self, mode='rU', **kwargs)
         try:
-            return f.read().replace('\r\n', '\n')
+            return f.read()
         finally:
             f.close()
 
